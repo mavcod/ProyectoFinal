56f34332f40cbdb0a4059e8d7aeac39327fa5a6a
edsf5 <edsf5@hotmail.com>
2015-11-20 17:34:24 -0500

    se hace el promedio de los valores de los pixeles para hacer una imagen a escal de grises a modo de ejemplo de como recorrer la matriz

--- Main/Main.c
+++ Main/Main.c
@@ -8,6 +8,24 @@ printf( "Principal Main \n" );
 ///lectura de imagen
  IMAGE *img;
     loadBMP("lena.bmp", &img);
+int i;
+//variables de entrada a la funcion
+uint8_t Rpixel;
+uint8_t Bpixel;
+uint8_t Gpixel;
+//recorriendo la imagen
+for (i = 0; i < img->width*img->height; i++) {
+Rpixel=img->data[i].r;
+Bpixel=img->data[i].g;
+Gpixel=img->data[i].b;
+//enviando las variables a las funciones.
+uint8_t avg = (Rpixel+ Bpixel + Gpixel)/3;
+//prototipado funcion promedio
+img->data[i].r = avg;
+img->data[i].g = avg;
+img->data[i].b = avg;
+}
+writeBMP("demo.bmp", img);
 ///
 lecturaRGB();
 maxim();
